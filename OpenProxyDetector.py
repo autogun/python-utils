035d318c3b7d736cbbff1f8b076ef480d0f1bbff
autogun <autogun@gmail.com>
2018-06-29 15:22:45 +0300

    Update OpenProxyDetector.py

--- OpenProxyDetector.py
+++ OpenProxyDetector.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 from botocore.vendored import requests
 from botocore.exceptions import ClientError
 from boto3.dynamodb.conditions import Key, Attr
@@ -12,6 +10,7 @@ import sys
 DYNAMODB_TABLE = '<table_name>'
 DYNAMODB_REGION = '<aws_region>'
 URL_CHECK = 'http://example.com'
+URL_CHECK_PAYLOAD = 'OK'
 TO_EMAIL = '<to_email_address>'
 FROM_EMAIL = 'OPEN PROXY DETECTOR <omg@alarm.com>'
 REPLY_TO = '<reply_to_email_address>'
@@ -50,7 +49,7 @@ def get_proxy_list():
     scan = table.scan()
     hostnames_list = []
     for i in scan['Items']:
-        hostnames_list += [x for x in i["hostnames"] if 'mgmt' not in x]
+        hostnames_list += [x for x in i["hostnames"] if '<string>' not in x] # Exlude hostname with with specific string
 
     return hostnames_list
     
@@ -60,19 +59,23 @@ def req_get(proxyhost):
         proxies = { 'http': 'http://{}:8080'.format(proxyhost),
                     'https': 'http://{}:8080'.format(proxyhost) }
         user_agent_headers = [ {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'},
-                            {'User-Agent': 'OpenProxyDetector CPython/3.6.1' } ]
+                            {'User-Agent': 'CloudOpenProxyDetector CPython/3.6.1' } ]
         requests.packages.urllib3.disable_warnings()
+        bypassed_requests = []
         for headers in user_agent_headers:
-            req = requests.get(URL_CHECK, proxies=proxies, headers=headers, timeout=1, verify=False)
-        return {'host': proxyhost, 'status': (req.text[:30])}
+            req = requests.get(URL_CHECK, proxies=proxies, headers=headers, timeout=2, verify=False)
+            bypassed_requests.append({'host': proxyhost, 'status': (
+                req.text[:30]), 'code': req.status_code, 'user-aget': headers['User-Agent']})
+        print(bypassed_requests)
+        return bypassed_requests
         
     except requests.exceptions.Timeout:
         print('Http GET request to {} timedout'.format(proxyhost))
-        return {'host': proxyhost, 'status': 'timeout'}
+        return [{'host': proxyhost, 'status': 'timeout'}]
         
     except requests.exceptions.RequestException as err:
         print('Http GET request to {} encountered an error: {}'.format(proxyhost, err))
-        return {'host': proxyhost, 'status': 'N/A'}
+        return [{'host': proxyhost, 'status': 'N/A'}]
         
         
 async def run_openproxy_detect():
@@ -85,9 +88,9 @@ async def run_openproxy_detect():
         ]
         openproxy_detected = []
         for response in await asyncio.gather(*futures):
-            # print(response)
-            if response.get('status') == 'OK':
-                openproxy_detected.append(response.get('host'))
+            for item in response:
+                if item.get('status') == URL_CHECK_PAYLOAD:
+                    openproxy_detected.append(item.get('host'))
         
         if len(openproxy_detected) > 0:
             send_email_ses(openproxy_detected)
@@ -96,4 +99,4 @@ async def run_openproxy_detect():
 def main(*_):
     loop = asyncio.get_event_loop()
     loop.run_until_complete(run_openproxy_detect())
-    
+    loop.close()
